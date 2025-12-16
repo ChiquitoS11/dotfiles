a072a057b8eaba5ae4b7b268e03278d9cd621cc8
ChiquitoS11 <142192480+ChiquitoS11@users.noreply.github.com>
2025-12-16 05:25:48 +0100

    installer fonts script

---
+++ fonts/installer-fonts.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+exec > /dev/null 2>&1
+set -e
+
+FONTS=(
+	"Agave"
+	"Gohu"
+	"JetBrainsMono"
+	"FiraCode"
+	"Hack"
+	"CascadiaCode"
+	"UbuntuMono"
+	"Hermit"
+)
+
+BASE_URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download"
+FONT_ROOT="$HOME/.local/share/fonts"
+TMP_BASE="/tmp/nerd-fonts"
+
+mkdir -p "$FONT_ROOT" "$TMP_BASE"
+
+for FONT in "${FONTS[@]}"; do
+    FONT_DIR="$FONT_ROOT/$FONT"
+
+    if [ -d "$FONT_DIR" ] && ls "$FONT_DIR"/*.ttf >/dev/null 2>&1; then
+        continue
+    fi
+
+    TMP_DIR="$TMP_BASE/$FONT"
+    mkdir -p "$TMP_DIR"
+
+
+    if ! curl -fL "$BASE_URL/$FONT.zip" -o "$TMP_DIR/$FONT.zip"; then
+        rm -rf "$TMP_DIR"
+        continue
+    fi
+
+
+    unzip -q "$TMP_DIR/$FONT.zip" -d "$TMP_DIR"
+
+    mkdir -p "$FONT_DIR"
+    find "$TMP_DIR" \( -name "*.ttf" -o -name "*.otf" \) -exec cp -n {} "$FONT_DIR" \;
+
+    rm -rf "$TMP_DIR"
+done
+
+
+fc-cache -f
--- installer.sh
+++ installer.sh
@@ -8,13 +8,24 @@ mkdir -p "$HOME/.config"
 
 
 # ACTUALIZAR OS
+echo "Actualizando OS..."
 sudo pacman -Syu > /dev/null
 echo "Sistema operativo actualizado con exito."
+echo ""
+
+
+# INSTALAR FUENTES
+echo "Instalado fuentes..."
+bash "./fonts/installer-fonts.sh"
+echo "Las fuentes se han instalado con exito"
+echo ""
 
 
 # INSTALAR GIT
+echo "Instalando git.."
 sudo pacman -S --needed base-devel git --noconfirm > /dev/null
-echo "Instalacion de git completada con exito"
+echo "Git se ha instalado con exito"
+echo ""
 
 
 # CONFIGURAR GIT (opcional)
@@ -23,9 +34,11 @@ read -p "Desea configurar git? Puede hacerlo manualmente luego... (yes/no) " GIT
 if [[ "${GITCONFIG,,}" == "yes" ]]; then
 	bash "./git-signature/create_signature.sh"
 fi
+echo ""
 
 
 # INSTALAR YAY
+echo "Instalando yay"
 if ! command -v yay > /dev/null; then
 	mkdir -p "./yay"
 	git clone https://aur.archlinux.org/yay.git "./yay/"
@@ -34,20 +47,29 @@ if ! command -v yay > /dev/null; then
 		makepkg -si
 	)
 fi
+echo "Yay se ha instalado con exito"
+echo ""
 
 
 # INSTALAR ZSH 
+echo "Instalando ZSH..."
 yay -S zsh --noconfirm > /dev/null
 chsh -s /usr/bin/zsh
+echo "ZSH se ha instalado con exito"
+echo ""
 
 
 # INSTALAR OH-MY-ZSH
+echo "Instalando OH-MY-ZSH..."
 if [[ ! -d "$HOME/.oh-my-zsh" ]]; then
     RUNZSH=no CHSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
-echo "oh-my-zsh se ha instalado con exito."
+echo "OH-MY-ZSH se ha instalado con exito."
+echo ""
+
 
 # INSTALAR PLUGINS PARA OH-MY-ZSH
+echo "Instalando plugins para OH-MY-ZSH..."
 ZSH_PATH="$HOME/.oh-my-zsh/custom/plugins"
 
 if [[ ! -d "$ZSH_PATH/zsh-autosuggestions" ]]; then
@@ -56,18 +78,27 @@ fi
 if [[ ! -d "$ZSH_PATH/zsh-syntax-highlighting" ]]; then
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_PATH/zsh-syntax-highlighting/"
 fi
-echo "Plugins instalados con exito"
+echo "Plugins OH-MY-ZSH instalados con exito"
+echo ""
+
 
 # INSTALAR STARSHIP (opcional)
+echo "Instalando starship..."
 yay -S starship --noconfirm > /dev/null
 starship preset pastel-powerline -o ~/.config/starship.toml
+echo "Starship instalado con exito"
+echo ""
 
 
 # INSTALAR STOW
+echo "Instalando stow..."
 yay -S stow --noconfirm > /dev/null
+echo "Stow se ha instalado con exito"
+echo ""
 
 
 # CONFIGURAR STOW
+echo "Configurando Stow..."
 if [[ -e "$HOME/.zshrc" ]]; then # .zshrc
 	rm "$HOME/.zshrc"
 fi
@@ -83,12 +114,16 @@ fi
 	cd "$HOME/dotfiles" 
 	stow kitty
 )
+echo "Stow configurado con exito"
+echo ""
 
 
 # Aplicaciones extra
+echo "Instalando paquetes de ayuda... (lsd, neovim)"
 yay -S lsd --noconfirm > /dev/null
 sudo yay -S neovim --noconfirm > /dev/null
-
+echo "Paquetes de ayuda instalados con exito"
+echo ""
 
 # Informar de la instalacion exitosa
 echo "---------------------------------"
